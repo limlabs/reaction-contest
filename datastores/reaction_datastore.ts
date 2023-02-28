ee7345982b95579cc604b038f71756b10f921412
Austin Montoya <austin.r.montoya@gmail.com>
2023-02-28 15:17:53 -0800

    Co-authored-by: Nathan Burgess <nathburg@users.noreply.github.com>

--- datastores/reaction_datastore.ts
+++ datastores/reaction_datastore.ts
@@ -1,7 +1,5 @@
 import { DefineDatastore, Schema } from "deno-slack-sdk/mod.ts";
-import { SlackAPIClient } from "https://deno.land/x/deno_slack_api@1.5.0/types.ts";
 import { appPrefix } from "../core/config.ts";
-import { ReactionEvent } from "../domain/reaction.ts";
 
 export const ReactionDatastoreName = `${appPrefix}-reactions`;
 
@@ -19,43 +17,3 @@ export const ReactionDatastoreSchema = {
 };
 
 export const ReactionDatastore = DefineDatastore(ReactionDatastoreSchema);
-
-export const saveReaction = async (
-  client: SlackAPIClient,
-  reaction: ReactionEvent,
-) => {
-  const response = await client.apps.datastore.put({
-    datastore: ReactionDatastoreName,
-    item: reaction,
-  });
-
-  if (!response.ok) {
-    throw new Error(
-      `failed to get reactions from datastore: ${response.error}`,
-    );
-  }
-};
-
-// TODO: Ensure we return as many results as possible (for now)
-// Eventually we will need to handle pagination.
-export const getReactionsSince = async (
-  client: SlackAPIClient,
-  since: number,
-) => {
-  const response = await client.apps.datastore.query<
-    typeof ReactionDatastoreSchema
-  >({
-    datastore: ReactionDatastoreName,
-    expression: "#timestamp > :last_updated",
-    expression_attributes: { "#timestamp": "timestamp" },
-    expression_values: { ":last_updated": since },
-  });
-
-  if (!response.ok) {
-    throw new Error(
-      `failed to get reactions from datastore: ${response.error}`,
-    );
-  }
-
-  return response.items.map((item) => item as ReactionEvent);
-};
