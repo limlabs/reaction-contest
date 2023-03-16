bb7e875a9ec7de19f12f9ab761ea986ebf429627
Nathan Burgess <nathans.email.address@gmail.com>
2023-03-16 16:22:21 -0700

    changes app name and icon

--- manifest.ts
+++ manifest.ts
@@ -13,10 +13,9 @@ import UpdateChannelsWorkflow from "./workflows/update_channels_workflow.ts";
  * https://api.slack.com/future/manifest
  */
 export default Manifest({
-  name: "reaction-contest",
-  description:
-    "A sample that demonstrates using a function, workflow and trigger to send a greeting",
-  icon: "assets/default_new_app_icon.png",
+  name: "Reaction Contest",
+  description: "A leaderboard that tracks the most popular emoji reactions.",
+  icon: "assets/cry-cat.png",
   workflows: [
     HandleReactionAddedWorkflow,
     UpdateLeaderboardWorkflow,
