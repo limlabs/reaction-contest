496001cf28ba02cd358a9240603f0544b387fb62
Austin Montoya <austin.r.montoya@gmail.com>
2023-03-25 19:10:15 -0700

    make the messages more fun

--- functions/view_leaderboard.ts
+++ functions/view_leaderboard.ts
@@ -17,26 +17,41 @@ export const ViewLeaderboardFunctionDefinition = DefineFunction({
   },
 });
 
+const defaultMessage = ":fire: *Reaction Contest Leaderboard* :fire:\n";
+const maxResults = 10;
+
+export const getDisplayPlace = (index: number) => {
+  if (index > 9) {
+    return (index + 1).toString();
+  }
+
+  const displayPlace = (index + 1).toString();
+
+  if (displayPlace.endsWith("1")) {
+    let out = `${displayPlace}st`;
+    if (index === 0) {
+      out = `${out}`;
+    }
+
+    return out;
+  }
+
+  if (displayPlace.endsWith("2")) {
+    return `${displayPlace}nd`;
+  }
+
+  if (displayPlace.endsWith("3")) {
+    return `${displayPlace}rd`;
+  }
+
+  return `${displayPlace}th`;
+};
+
 export default SlackFunction(
   ViewLeaderboardFunctionDefinition,
   async ({ client }) => {
     const { data: leaderboardData } = await getLeaderboardInfo(client);
     if (leaderboardData.length === 0) {
-      return {
-        outputs: { leaderboardMessage: "React to some posts!" },
-      };
-    }
-    let leaderboardMessage = "*Reaction Contest Leaderboard*\n";
-    for (let i = 0; i < leaderboardData.length; ++i) {
-      const item = leaderboardData[i];
-      if (item.count === 0) {
-        break;
-      }
-      leaderboardMessage += `${i + 1}. :${item.reaction}: was used ${
-        item.count === 1 ? "1 time" : `${item.count} times`
-      }\n`;
-    }
-    if (leaderboardMessage === "*Reaction Contest Leaderboard*\n") {
       return {
         outputs: {
           leaderboardMessage:
@@ -44,6 +59,21 @@ export default SlackFunction(
         },
       };
     }
+    let leaderboardMessage = defaultMessage;
+
+    for (let i = 0; i < Math.min(leaderboardData.length, maxResults); ++i) {
+      const item = leaderboardData[i];
+      if (item.count === 0) {
+        break;
+      }
+
+      const count = item.count === 1 ? "1 time" : `${item.count} times`;
+
+      leaderboardMessage += `${
+        getDisplayPlace(i)
+      } - :${item.reaction}: - used ${count}\n`;
+    }
+
     return { outputs: { leaderboardMessage } };
   },
 );
